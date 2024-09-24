1de55421c373a9e799f4b35def0c58ac33bf28d3
unknown <dewanisarman88@gmail.com>
2024-09-24 12:05:08 +0600

    blog click to other html open has compleated

---
+++ js/sectionOpen.js
@@ -0,0 +1,17 @@
+document.getElementById("donate-open-btn")
+.addEventListener('click',function(){
+ShowSectionById ("donate-section");
+
+
+})
+
+document.getElementById("history-open-btn")
+.addEventListener('click',function(){
+    ShowSectionById ("history-section");
+
+})
+document.getElementById("blog-open-btn")
+.addEventListener('click',function(){
+    window.location.href = "/blog.html"
+
+})
--- js/utilities.js
+++ js/utilities.js
@@ -12,6 +12,7 @@ function getTextValueFile(id){
     return textAmountNumber;
 }
 
+// this function for date and time
 function donateHistory (donate,data){
     const time = new Date()
    const donation ={donate,text:data,time};
@@ -19,3 +20,11 @@ function donateHistory (donate,data){
 
 }
 
+function ShowSectionById (id){
+    document.getElementById("history-section").classList.add('hidden');
+    document.getElementById("donate-section").classList.add('hidden');
+    // document.getElementById("donate-section").classList.add('hidden');
+
+
+    document.getElementById(id).classList.remove('hidden');
+}
