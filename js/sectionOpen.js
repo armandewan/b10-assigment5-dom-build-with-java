dec349ca6125ed76bc0f57769332d323bf8429e6
unknown <dewanisarman88@gmail.com>
2024-09-24 16:27:34 +0600

    header include the blog

--- js/sectionOpen.js
+++ js/sectionOpen.js
@@ -9,9 +9,14 @@ document.getElementById('donate-open-btn').classList.add('bg-lime-400');
 document.getElementById("history-open-btn")
 .addEventListener('click',function(){
     ShowSectionById ("history-section");
-  document.getElementById('donate-open-btn').classList.remove('bg-lime-400 ');
+  document.getElementById('donate-open-btn').classList.remove('bg-lime-400');
   document.getElementById('history-open-btn').classList.add('bg-lime-400');
-
+  if(history.length <= 0){
+    document.getElementById("empty-text-history").innerText = "You do not any donation ";
+  } else{
+    document.getElementById("empty-text-history").innerText = "";
+  }
+  
 })
 document.getElementById("blog-open-btn")
 .addEventListener('click',function(){
