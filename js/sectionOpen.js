9be3d39e51465d1d9c221644c2012793dea188d0
unknown <dewanisarman88@gmail.com>
2024-09-24 19:40:26 +0600

    finish project

--- js/sectionOpen.js
+++ js/sectionOpen.js
@@ -12,12 +12,12 @@ document.getElementById("history-open-btn")
   document.getElementById('donate-open-btn').classList.remove('bg-lime-400');
   document.getElementById('history-open-btn').classList.add('bg-lime-400');
   if(history.length <= 0){
-    document.getElementById("empty-text-history").innerText = "You do not any donation ";
+    document.getElementById("empty-text-history").innerText = "You do not any donation... ";
   } else{
     document.getElementById("empty-text-history").innerText = "";
   }
-  
 })
+
 document.getElementById("blog-open-btn")
 .addEventListener('click',function(){
     window.location.href = "/blog.html";
--- js/utilities.js
+++ js/utilities.js
@@ -5,21 +5,17 @@ const inputNumber = parseFloat(inputAdd);
 return inputNumber;
 
 }
-
 function getTextValueFile(id){
     const textAmount = document.getElementById(id).innerText;
     const textAmountNumber = parseFloat(textAmount);
     return textAmountNumber;
 }
-
 // this function for date and time
 function donateHistory (donate,data){
     const time = new Date()
    const donation ={donate,text:data,time};
    history.push(donation);
-
 }
-
 function ShowSectionById (id){
     document.getElementById("history-section").classList.add('hidden');
     document.getElementById("donate-section").classList.add('hidden');
